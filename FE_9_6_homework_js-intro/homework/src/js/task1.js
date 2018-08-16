4199c1af3d725cad5324e95298921db37a337f04
TarasKhomko <taras.khomko@gmail.com>
2018-08-16 16:51:55 +0300

    fixed homework 6

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,9 +1,12 @@
 let price = prompt( 'Enter price');
 let discount = prompt( 'Enter discount');
+price = Math.trunc(parseFloat(price) * 100) / 100;
+discount = Math.trunc(parseFloat(discount) * 100) / 100;
+
 let maxDiscount = 100;
 let priceDicount = price - price * ( discount / 100);
 let saved = price - priceDicount;
-  console.log( price < 0 || discount > maxDiscount ? 'Invalid data': 'Price without discount: ' +price+
+  console.log( price < 0 || discount > maxDiscount ? 'Invalid data': 'Price without discount: ' + price +
   '\n' + 'Discount: ' + discount+'%'+
   '\n' + 'Price with discount: '+ parseFloat(priceDicount.toFixed(2))+
   '\n' + 'Saved: ' + parseFloat(saved.toFixed(2))
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,12 +1,17 @@
-const a = prompt( 'Enter length a');
-const b = prompt( 'Enter length b');
-const angle = prompt( 'Enter angle');
-const deg = 180;
-const g = 0.5;
-const angleRad = angle*(Math.PI/deg);
-const c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2) - 2*a*b* Math.abs(Math.cos(angleRad)) );
-const s = g*a*b*Math.sin(angleRad);
-const p = parseFloat(a) + parseFloat(b) + parseFloat(c);
+let a = prompt( 'Enter length a');
+let b = prompt( 'Enter length b');
+let angle = prompt( 'Enter angle');
+a = Math.trunc(parseFloat(a) * 100) / 100;
+b = Math.trunc(parseFloat(b) * 100) / 100;
+console.log(a);
+
+angle = parseFloat(angle);
+let deg = 180;
+let g = 0.5;
+let angleRad = angle*(Math.PI/deg);
+let c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2) - 2*a*b* Math.abs(Math.cos(angleRad)) );
+let s = g*a*b*Math.sin(angleRad);
+let p = parseFloat(a) + parseFloat(b) + parseFloat(c);
 console.log( a<= 0 || b<=0 || angle <=0 || angle >=deg ? 'Invalid data' : 'c length: ' + parseFloat(c.toFixed(2)) +'\n'
 	+ 'Triangle square: '+ parseFloat(s.toFixed(2)) + '\n'
 	+ 'Triangle perimeter: ' + parseFloat(p.toFixed(2))
