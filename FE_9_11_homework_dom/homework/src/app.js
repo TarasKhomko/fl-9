9a004b49bcc4df684f48c8fdbade4df5db47fad1
TarasKhomko <taras.khomko@gmail.com>
2018-09-03 22:07:29 +0300

    homework 11

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,44 +1,45 @@
 let parent = document.getElementById('item-icon');
-
-document.getElementById("btn").disabled  = true;
-
-document.getElementById("input-text").onkeyup = function(){
-	btnActive();
+const max = 10;
+const zero = 0;
+document.getElementById('btn').disabled = true;
+document.getElementById('input-text').onkeyup = function() {
+  btnActive();
 }
-
-function btnActive(){
-	if (!document.getElementById("input-text").value) {
-		
-		document.getElementById("btn").disabled  = true;
-	}
-	else {
-		document.getElementById("btn").disabled = false;
-	}
-
+function btnActive() {
+  if (!document.getElementById('input-text').value) {
+    document.getElementById('btn').disabled = true;
+  } else {
+    document.getElementById('btn').disabled = false;
+  }
 }
-  
-function add_icon(){
-
-	let text = document.getElementById("input-text").value;
-    let li = document.createElement("li");
-    li.setAttribute("class", "item-li" );
-  	let btn_done = document.createElement("button");
-    let btn_delete = document.createElement("button");
-
-    btn_delete.setAttribute("class", "btn-delete");
-    btn_delete.innerHTML = "<i class=\"material-icons\">delete</i>";
-    btn_done.innerHTML = "<i class=\"material-icons\">check_box_outline_blank</i>"
-    if (text.length > 0  && parent.getElementsByTagName('li').length <=9) {
-    let t = document.createTextNode(text);
-    li.appendChild(btn_done);
-    li.appendChild(t);
-    li.appendChild(btn_delete);
-    parent.appendChild(li);
-    let text2 = document.getElementById("input-text").value = '';
-    document.getElementById("btn").disabled  = true;
-    }
+function addContent() {
+  const li = document.createElement('li');
+  li.setAttribute('class', 'item-li');
+  let text = document.getElementById('input-text').value;
+  let btn_done = document.createElement('button');
+	btn_done.setAttribute('class', 'btn-done');
+	btn_done.innerHTML = '<i class="material-icons">check_box_outline_blank</i>'
+  let btn_delete = document.createElement('button');
+  btn_delete.setAttribute('class', 'btn-delete');
+  btn_delete.innerHTML = '<i class="material-icons">delete</i>';
+	let t = document.createTextNode(text);
+  if (text.length > zero && parent.getElementsByTagName('li').length < max) {
+		li.appendChild(btn_done);
+		li.appendChild(t);
+		li.appendChild(btn_delete);
+		parent.appendChild(li);
+    document.getElementById('input-text').value = '';
+    document.getElementById('btn').disabled = true;
+    btn_done.onclick = event => {
+    btn_done.innerHTML = '<i class="material-icons">check_box</i>'
+};
+ btn_delete.onclick = event => {
+ parent.removeChild(li);
+};
+  } else {
+  document.getElementById('text-limit').style.display = 'block';
+	document.getElementById('input-text').disabled = true;
+	document.getElementById('input-text').value = '';
+	document.getElementById('btn').disabled = true;
+  }
 }
-
-document.getElementsByClassName("btn-delete").addEventListener("click", function(){
-    alert("Hello World!");
-});
